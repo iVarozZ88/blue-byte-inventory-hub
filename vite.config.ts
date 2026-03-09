9de60c20a235ca50b23745c05219620a43baa8a6
fabrizzio0608 <fabrizzio.franco@mcimanager.com>
2026-03-09 09:25:58 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
+  
   server: {
     host: "::",
     port: 8080,
